bd223e59afde2e6c4b55292241c702a2b786bee5
Akshit Saini <sainiakshit2941@gmail.com>
2026-07-27 23:36:34 +0530

    fanout exchange implementation

--- distributed-chat-system/src/main/java/com/example/distributed_chat_system/config/RabbitConfig.java
+++ distributed-chat-system/src/main/java/com/example/distributed_chat_system/config/RabbitConfig.java
@@ -1,15 +1,27 @@
 package com.example.distributed_chat_system.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.core.Queue;
-
 
 @Configuration
 public class RabbitConfig {
 
     @Bean
+    public FanoutExchange chatExchange() {
+        return new FanoutExchange("chat_exchange", true, false);
+    }
+
+    @Bean
     public Queue chatQueue(){
         return new Queue("chat_queue", true);
+    }
+
+    @Bean
+    public Binding binding(Queue chatQueue, FanoutExchange chatExchange) {
+        return BindingBuilder.bind(chatQueue).to(chatExchange);
     }
 }
